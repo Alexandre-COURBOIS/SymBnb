1293a0eb88adf53e969125cda66e09ac86abc4eb
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2020-09-24 12:45:34 +0200

    modification for password is now available with error

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\PasswordUpdate;
 use App\Entity\User;
 use App\Form\AccountType;
+use App\Form\PasswordUpdateType;
 use App\Form\RegistrationType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -62,7 +65,7 @@ class AccountController extends AbstractController
 
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
 
             $hash = $encoder->encodePassword($user, $user->getPassword());
             $user->setPassword($hash);
@@ -78,7 +81,7 @@ class AccountController extends AbstractController
             return $this->redirectToRoute('account_login');
         }
 
-        return  $this->render('account/registration.html.twig', [
+        return $this->render('account/registration.html.twig', [
             'form' => $form->createView()
         ]);
     }
@@ -90,7 +93,8 @@ class AccountController extends AbstractController
      *
      * @return Response
      */
-    public function profil(Request $request,EntityManagerInterface $manager) {
+    public function profil(Request $request, EntityManagerInterface $manager)
+    {
         $user = $this->getUser();
 
         $form = $this->createForm(AccountType::class, $user);
@@ -102,10 +106,64 @@ class AccountController extends AbstractController
             $manager->persist($user);
             $manager->flush();
 
+            $this->addFlash(
+                'success',
+                'Les données du profil ont été modifées avec succès.');
         }
 
         return $this->render('account/profil.html.twig', [
             'form' => $form->createView()
         ]);
     }
+
+    /**
+     *Permet de modifier le mot de passe
+     *
+     * @Route("/account/password-update", name="account_password")
+     *
+     * @return Response
+     */
+    public function updatePassword(Request $request, EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder)
+    {
+
+        $passwordUpdate = new PasswordUpdate();
+
+        $user = $this->getUser();
+
+        $form = $this->createForm(PasswordUpdateType::class, $passwordUpdate);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            if (password_verify($passwordUpdate->getOldPassword(), $user->getPassword())) {
+
+                $newPassword = $passwordUpdate->getNewPassword();
+                $hash = $encoder->encodePassword($user, $newPassword);
+
+                $user->setpassWord($hash);
+
+                $manager->persist($user);
+                $manager->flush();
+
+                $this->addFlash(
+                    'success',
+                    'Votre mot de passe à bien été modifié.');
+
+                return $this->redirectToRoute('account_profil');
+
+            } else {
+                //gestion de l'erreur, l'expression ci dessous me donne l'accès au champ old password via api form en get
+                $form->get('oldPassword')->addError(new FormError('Le mot de passe que vous avez saisis n\'est pas valide'));
+
+            }
+        }
+
+        return $this->render('account/password.html.twig', [
+
+            'form' => $form->createView()
+
+        ]);
+    }
+
 }
